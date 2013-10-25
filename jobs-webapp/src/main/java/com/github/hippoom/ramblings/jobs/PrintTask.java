28446a68ec1f3a4c4771561b3683a69fbf9a7b8b
hippoom <hippoom@gmail.com>
2013-10-25 12:37:21 +0800

    Add job fail scenario

--- jobs-webapp/src/main/java/com/github/hippoom/ramblings/jobs/PrintTask.java
+++ jobs-webapp/src/main/java/com/github/hippoom/ramblings/jobs/PrintTask.java
@@ -3,7 +3,13 @@ package com.github.hippoom.ramblings.jobs;
 public class PrintTask implements Runnable {
 	@Override
 	public void run() {
-		System.err.println(Thread.currentThread().getName() + " Hello world");
+		String threadName = Thread.currentThread().getName();
+
+		if (threadName.indexOf("-1") > 0) {
+			throw new IllegalArgumentException(threadName + " Fail");
+		} else {
+			System.err.println(threadName + " Hello world");
+		}
 	}
 
 }
